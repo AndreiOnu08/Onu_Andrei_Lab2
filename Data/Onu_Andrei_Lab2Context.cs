b4d329298ad4bc5ca6a94f12e3cfa1269131d925
AndreiOnu08 <andrei.onu@stud.ubbcluj.ro>
2022-10-20 21:19:04 +0300

    Add project files.

---
+++ Data/Onu_Andrei_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nume_Pren_Lab2.Models;
+
+namespace Onu_Andrei_Lab2.Data
+{
+    public class Onu_Andrei_Lab2Context : DbContext
+    {
+        public Onu_Andrei_Lab2Context (DbContextOptions<Onu_Andrei_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nume_Pren_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
